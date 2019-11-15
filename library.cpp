5b4b769293d1a5b5480bf11012fbaea9fda72757
ayushsapkota <57805036+ayushsapkota@users.noreply.github.com>
2019-11-15 17:36:16 +0100

    project
    my first project

--- library.cpp
+++ library.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"please enter the number"<<endl;
+    cin>>number;
+    {
+    if (number<=0)
+        cout<<"negative number"<<endl;
+    else{
+        cout<<"positive number"<<endl;
+    }
+    }
+    return 0;
+}
